adec9377de74628bd3592650d3db226d57d46fb1
Sergey Dedkov <dsv.mail@yandex.ru>
2023-01-20 15:40:03 +0600

    add rollup-plugin-visualizer, use with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://dsvgit.github.io/mide/',
-  plugins: [react()],
+  plugins: [react(), visualizer()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
