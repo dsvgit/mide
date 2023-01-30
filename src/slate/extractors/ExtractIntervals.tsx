b2c537f299676e1117cd8aeb10389ac811ad2193
Sergey Dedkov <dsv.mail@yandex.ru>
2023-01-31 01:08:28 +0600

    optimize extractors with useMemo hook

--- src/slate/extractors/ExtractIntervals.tsx
+++ src/slate/extractors/ExtractIntervals.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { Element } from 'slate'
 import { useSlate } from 'slate-react'
 
@@ -5,39 +6,34 @@ import { useSlate } from 'slate-react'
 // set editor.intervalsStarts with set of these intervals start index (for optimization)
 export const ExtractIntervals = () => {
   const editor = useSlate()
-  const children = editor.children
 
-  const intervals = new Map<string, [number, number][]>()
-  const intervalsStarts = new Map<string, Set<number>>()
-  let prev = null
-  // the loop has children.length + 1 iterations in order to close the last interval
-  for (let i = 0; i < children.length + 1; i++) {
-    const element = children[i] as Element
+  useMemo(() => {
+    const intervals = new Map<string, [number, number][]>()
+    let prev = null
+    // the loop has children.length + 1 iterations in order to close the last interval
+    for (let i = 0; i < editor.children.length + 1; i++) {
+      const element = editor.children[i] as Element
 
-    if (!prev || !element || prev.type !== element.type) {
-      if (prev) {
-        // update the end of an interval
-        intervals.get(prev.type)!.slice(-1)[0][1] = i - 1
+      if (!prev || !element || prev.type !== element.type) {
+        if (prev) {
+          // update the end of an interval
+          intervals.get(prev.type)!.slice(-1)[0][1] = i - 1
+        }
+
+        if (element) {
+          if (!intervals.has(element.type)) {
+            intervals.set(element.type, [])
+          }
+          // push the start of an interval
+          intervals.get(element.type)!.push([i, 0])
+        }
       }
 
-      if (element) {
-        if (!intervals.has(element.type)) {
-          intervals.set(element.type, [])
-        }
-        // push the start of an interval
-        intervals.get(element.type)!.push([i, 0])
-
-        if (!intervalsStarts.has(element.type)) {
-          intervalsStarts.set(element.type, new Set())
-        }
-        intervalsStarts.get(element.type)!.add(i)
-      }
+      prev = element
     }
 
-    prev = element
-  }
-
-  editor.intervals = intervals
+    editor.intervals = intervals
+  }, [editor.children])
 
   return null
 }
--- src/slate/extractors/ExtractRanges.tsx
+++ src/slate/extractors/ExtractRanges.tsx
@@ -8,6 +8,7 @@ import 'prismjs/components/prism-python'
 import 'prismjs/components/prism-php'
 import 'prismjs/components/prism-sql'
 import 'prismjs/components/prism-java'
+import { useMemo } from 'react'
 import { Node, Point, Range } from 'slate'
 import { ReactEditor, useSlate } from 'slate-react'
 
@@ -29,137 +30,140 @@ class PrismTokenNode {
 export const ExtractRanges = (options: Partial<SlateEditorProps>) => {
   const editor = useSlate()
 
-  const language = getLanguage(options.ext || '')
-  const ranges = new Map<string, Range[]>()
+  useMemo(() => {
+    const language = getLanguage(options.ext || '')
+    const ranges = new Map<string, Range[]>()
 
-  if (!language) {
-    editor.ranges = ranges
-    return null
-  }
-
-  const intervals = editor.intervals!.get(CodeLineType) || [] // work only with code line intervals
-  for (const interval of intervals) {
-    const text = editor.children
-      .slice(interval[0], interval[1] + 1)
-      .map((element) => Node.string(element))
-      .join('\n')
-    const tokens = Prism.tokenize(text, Prism.languages[language])
-
-    const stack: PrismTokenNode[] = []
-
-    // fill initial stack with reversed tokens
-    for (let i = tokens.length - 1; i >= 0; i--) {
-      stack.push(new PrismTokenNode(tokens[i], null))
+    if (!language) {
+      editor.ranges = ranges
+      return null
     }
 
-    let startOffset = 0
-    let index = interval[0]
-    while (stack.length) {
-      const currentTokenNode = stack.pop()!
-      const token = currentTokenNode.token
-      const length = token.length
+    const intervals = editor.intervals!.get(CodeLineType) || [] // work only with code line intervals
+    for (const interval of intervals) {
+      const text = editor.children
+        .slice(interval[0], interval[1] + 1)
+        .map((element) => Node.string(element))
+        .join('\n')
+      const tokens = Prism.tokenize(text, Prism.languages[language])
 
-      if (typeof token !== 'string' && Array.isArray(token.content)) {
-        // add reversed content (children) tokens to stack
-        for (let i = token.content.length - 1; i >= 0; i--) {
-          stack.push(new PrismTokenNode(token.content[i], currentTokenNode))
-        }
-        continue
+      const stack: PrismTokenNode[] = []
+
+      // fill initial stack with reversed tokens
+      for (let i = tokens.length - 1; i >= 0; i--) {
+        stack.push(new PrismTokenNode(tokens[i], null))
       }
 
-      // split string into lines to be sure that there are no multiline tokens
-      const str = typeof token === 'string' ? token : (token.content as string)
-      if (str !== '\n') {
-        const lines = str.split(/(\n)/)
-        if (lines.length > 1) {
-          // if it is multiline token split it add it back to stack each line separately in reversed order
-          for (let i = lines.length - 1; i >= 0; i--) {
-            const line = lines[i]
+      let startOffset = 0
+      let index = interval[0]
+      while (stack.length) {
+        const currentTokenNode = stack.pop()!
+        const token = currentTokenNode.token
+        const length = token.length
 
-            stack.push(
-              new PrismTokenNode(
-                typeof token === 'string'
-                  ? line
-                  : new Prism.Token(token.type, line, token.alias, line),
-                currentTokenNode
-              )
-            )
+        if (typeof token !== 'string' && Array.isArray(token.content)) {
+          // add reversed content (children) tokens to stack
+          for (let i = token.content.length - 1; i >= 0; i--) {
+            stack.push(new PrismTokenNode(token.content[i], currentTokenNode))
           }
           continue
         }
-      } else {
-        // if it is line break reset startOffset and increment index
-        index++
-        startOffset = 0
-        continue
-      }
 
-      // calculate endOffset
-      const endOffset = startOffset + length
+        // split string into lines to be sure that there are no multiline tokens
+        const str =
+          typeof token === 'string' ? token : (token.content as string)
+        if (str !== '\n') {
+          const lines = str.split(/(\n)/)
+          if (lines.length > 1) {
+            // if it is multiline token split it add it back to stack each line separately in reversed order
+            for (let i = lines.length - 1; i >= 0; i--) {
+              const line = lines[i]
 
-      // create types set based on token's and all ancestors' types
-      const types = new Set<string>(['token'])
-      let tokenNodeCursor: PrismTokenNode | null = currentTokenNode
-      while (tokenNodeCursor) {
-        const { token } = tokenNodeCursor
+              stack.push(
+                new PrismTokenNode(
+                  typeof token === 'string'
+                    ? line
+                    : new Prism.Token(token.type, line, token.alias, line),
+                  currentTokenNode
+                )
+              )
+            }
+            continue
+          }
+        } else {
+          // if it is line break reset startOffset and increment index
+          index++
+          startOffset = 0
+          continue
+        }
 
-        if (typeof token !== 'string') {
-          const type = token.type
-          const aliasArray = Array.isArray(token.alias)
-            ? token.alias
-            : [token.alias]
+        // calculate endOffset
+        const endOffset = startOffset + length
 
-          const isTag = type === 'tag' || aliasArray.includes('tag')
+        // create types set based on token's and all ancestors' types
+        const types = new Set<string>(['token'])
+        let tokenNodeCursor: PrismTokenNode | null = currentTokenNode
+        while (tokenNodeCursor) {
+          const { token } = tokenNodeCursor
 
-          if (types.has('script') && isTag) {
-            // skip "tag" type and following, if types already has "script" type
-            // matters for correct jsx, tsx attributes highlighting
-            break
+          if (typeof token !== 'string') {
+            const type = token.type
+            const aliasArray = Array.isArray(token.alias)
+              ? token.alias
+              : [token.alias]
+
+            const isTag = type === 'tag' || aliasArray.includes('tag')
+
+            if (types.has('script') && isTag) {
+              // skip "tag" type and following, if types already has "script" type
+              // matters for correct jsx, tsx attributes highlighting
+              break
+            }
+
+            types.add(type)
+
+            for (const alias of aliasArray) {
+              alias && types.add(alias)
+            }
           }
 
-          types.add(type)
+          tokenNodeCursor = tokenNodeCursor.parent
+        }
 
-          for (const alias of aliasArray) {
-            alias && types.add(alias)
+        // fill ranges map for decorate function
+        if (types.size) {
+          const key = ReactEditor.findKey(editor, editor.children[index])
+          if (!ranges.has(key.id)) {
+            ranges.set(key.id, [])
           }
+
+          const anchor: Point = {
+            basePath: [index, 0],
+            path: [],
+            offset: startOffset,
+          }
+          const focus: Point = {
+            basePath: [index, 0],
+            path: [],
+            offset: endOffset,
+          }
+
+          const range: Range = { anchor, focus }
+
+          for (const type of types) {
+            range[type] = true
+          }
+
+          ranges.get(key.id)!.push(range)
         }
 
-        tokenNodeCursor = tokenNodeCursor.parent
+        // move startOffset to end
+        startOffset = endOffset
       }
-
-      // fill ranges map for decorate function
-      if (types.size) {
-        const key = ReactEditor.findKey(editor, editor.children[index])
-        if (!ranges.has(key.id)) {
-          ranges.set(key.id, [])
-        }
-
-        const anchor: Point = {
-          basePath: [index, 0],
-          path: [],
-          offset: startOffset,
-        }
-        const focus: Point = {
-          basePath: [index, 0],
-          path: [],
-          offset: endOffset,
-        }
-
-        const range: Range = { anchor, focus }
-
-        for (const type of types) {
-          range[type] = true
-        }
-
-        ranges.get(key.id)!.push(range)
-      }
-
-      // move startOffset to end
-      startOffset = endOffset
     }
-  }
 
-  editor.ranges = ranges
+    editor.ranges = ranges
+  }, [editor.children])
 
   return null
 }
